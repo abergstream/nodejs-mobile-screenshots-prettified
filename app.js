4da3574422ea3d463e492c5a5b01718698dcafae
Andreas Bergström <andreas@textalkgroup.com>
2024-06-17 21:20:14 +0200

    Changed prompt type

--- app.js
+++ app.js
@@ -1,71 +1,107 @@
 const sharp = require("sharp");
 const process = require("process");
 const prompt = require("prompt-sync")();
+const prompts = require("prompts");
+(async () => {
+  const query = await prompts([
+    {
+      type: "text",
+      name: "color1",
+      message: "Color 1 (#777 if empty) ",
+    },
+    {
+      type: "text",
+      name: "color2",
+      message: "Color 1 (#CCC if empty) ",
+    },
+    {
+      type: "select",
+      name: "color",
+      message: "Pick a color",
+      choices: [
+        {
+          title: "Red",
+          description: "This option has a description.",
+          value: "#ff0000",
+        },
+        { title: "Green", value: "#00ff00" },
+        { title: "Yellow", value: "#ffff00", disabled: true },
+        { title: "Blue", value: "#0000ff" },
+      ],
+    },
+  ]);
 
-const createPhoneSvg = require("./src/service/phoneSvg.js");
-const createGradientSVG = require("./src/service/gradientSvg.js");
-let color1 = prompt("Color 1? (#777777 if empty) ");
-let color2 = prompt("Color 2? (#CCCCCC if empty) ");
-color1 = color1 ? color1 : "#777";
-color2 = color2 ? color2 : "#CCC";
+  const createPhoneSvg = require("./src/service/phoneSvg.js");
+  const createGradientSVG = require("./src/service/gradientSvg.js");
+  // let color1 = prompt("Color 1? (#777777 if empty) ");
+  // let color2 = prompt("Color 2? (#CCCCCC if empty) ");
 
-// Paths to the image
-const outputPath = "./testOutput2.png"; // Path for the output image
+  const color1 = query.color1 ? query.color1 : "#777";
+  const color2 = query.color2 ? query.color2 : "#CCC";
 
-const canvasWidth = 1080;
-const canvasHeight = 2400;
+  // Paths to the image
+  const outputPath = "./testOutput2.png"; // Path for the output image
 
-const phoneWidth = 700;
-const phoneHeight = 1300;
+  const canvasWidth = 1080;
+  const canvasHeight = 2400;
 
-// Create gradient background buffer
-const gradientSVG = createGradientSVG(
-  canvasWidth,
-  canvasHeight,
-  color1,
-  color2
-);
-// Create phoneSVG buffer
-const phoneSVG = createPhoneSvg(phoneWidth, phoneHeight);
+  const phoneWidth = 700;
+  const phoneHeight = 1300;
 
-sharp(Buffer.from(gradientSVG))
-  .png()
-  .toBuffer()
-  .then((gradientBuffer) => {
-    sharp(Buffer.from(phoneSVG))
-      .png()
-      .toBuffer()
-      .then((phoneBuffer) => {
-        // Create a blank canvas with the desired background color (white in this case)
-        sharp({
-          create: {
-            width: canvasWidth,
-            height: canvasHeight,
-            channels: 3, // No alpha channel
-            background: { r: 255, g: 255, b: 255 }, // White background
-          },
+  // Create gradient background buffer
+  const gradientSVG = createGradientSVG(
+    canvasWidth,
+    canvasHeight,
+    color1,
+    color2
+  );
+  // Create phoneSVG buffer
+  const phoneSVG = createPhoneSvg(
+    canvasWidth,
+    canvasHeight,
+    phoneWidth,
+    phoneHeight
+  );
+
+  sharp(Buffer.from(gradientSVG))
+    .png()
+    .toBuffer()
+    .then((gradientBuffer) => {
+      sharp(Buffer.from(phoneSVG))
+        .png()
+        .toBuffer()
+        .then((phoneBuffer) => {
+          // Create a blank canvas with the desired background color (white in this case)
+          sharp({
+            create: {
+              width: canvasWidth,
+              height: canvasHeight,
+              channels: 3, // No alpha channel
+              background: { r: 255, g: 255, b: 255 }, // White background
+            },
+          })
+            .composite([
+              { input: gradientBuffer, top: 0, left: 0 }, // Apply gradient background
+              {
+                input: phoneBuffer,
+                top: 0,
+                left: 0,
+              },
+            ])
+            .removeAlpha() // Removes alpha channel
+            .toFile(outputPath, (err, info) => {
+              if (err) {
+                console.error("Error merging images:", err);
+              } else {
+                console.log("Images merged successfully:", info);
+              }
+            });
         })
-          .composite([
-            { input: gradientBuffer, top: 0, left: 0 }, // Apply gradient background
-            {
-              input: phoneBuffer,
-              top: 300,
-              left: (canvasWidth - phoneWidth) / 2,
-            }, // Center the phone SVG horizontally
-          ])
-          .removeAlpha()
-          .toFile(outputPath, (err, info) => {
-            if (err) {
-              console.error("Error merging images:", err);
-            } else {
-              console.log("Images merged successfully:", info);
-            }
-          });
-      })
-      .catch((err) => {
-        console.error("Error creating phone buffer:", err);
-      });
-  })
-  .catch((err) => {
-    console.error("Error creating gradient buffer:", err);
-  });
+        .catch((err) => {
+          console.error("Error creating phone buffer:", err);
+        });
+    })
+    .catch((err) => {
+      console.error("Error creating gradient buffer:", err);
+    });
+})();
--- src/service/phoneSvg.js
+++ src/service/phoneSvg.js
@@ -1,15 +1,21 @@
-const createPhoneSvg = (width, height) => {
+const createPhoneSvg = (canvasWidth, canvasHeight, width, height) => {
   const strokeWidth = 30;
+
+  // Offset to calculate the center
+  const offset = strokeWidth / 2;
+
+  // Gets the actual size of the phone
   const phoneWidth = width - strokeWidth;
   const phoneHeight = height - strokeWidth;
-  return `
-  <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-  <g transform="rotate(45deg)">
 
-    <rect x="${strokeWidth / 2}" y="${
-    strokeWidth / 2
-  }" width="${phoneWidth}px" height="${phoneHeight}px" fill="none" stroke="black" stroke-linejoin="round" stroke-width="${strokeWidth}" rx="${strokeWidth}" ry="${strokeWidth}" />
-    </g>
+  // Gets the center of the image
+  const xPos = (canvasWidth - phoneWidth) / 2 - offset;
+
+  return `
+  <svg width="${canvasWidth}" height="${canvasHeight}" xmlns="http://www.w3.org/2000/svg">
+    <rect x="${xPos}" y="300" width="${phoneWidth}px" height="${phoneHeight}px"
+      fill="none" stroke="black" stroke-linejoin="round"
+           stroke-width="${strokeWidth}" rx="${strokeWidth}" ry="${strokeWidth}" />
   </svg>
 `;
 };
